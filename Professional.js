1798eafac27084e7cd74d2e6d1f580952c929736
Archivopdf <toni.diaz.gar77@gmail.com>
2022-07-25 10:59:36 +0200

    .

--- Professional.js
+++ Professional.js
@@ -26,8 +26,4 @@ var Professional = /** @class */ (function () {
     return Professional;
 }());
 exports.Professional = Professional;
-var a = new Professional("juan", 33, "terror", 89, 190, "azul", "verdes", ".", false, "japones", 1, "actor");
-var b = new Professional("pedro", 19, "accion", 55, 169, "castaño", "verdes", ".", false, "aleman", 0, "actor");
-var c = new Professional("maria", 55, "sci-fi", 88, 192, "rubio", "marron", ".", true, "americano", 2, "director");
-var d = new Professional("rebeca", 33, "accion", 59, 175, "rubio", "azul", ".", false, "mejicana", 0, "actor");
-a.getprofessional();
+
