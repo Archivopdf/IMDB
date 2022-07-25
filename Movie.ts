77d515f3c0235f64eab548ca62546a10a2c26dfa
dianacastano <109579084+dianacastano@users.noreply.github.com>
2022-07-25 11:49:47 +0200

    añadido de getter y setters

--- Movie.ts
+++ Movie.ts
@@ -5,7 +5,7 @@ class Movie {
     public title: string;
     public releaseYear: number;
     public actors: Professional[];
-    public nacionality: Professional[];
+    public nacionality: string;
     public writer: Professional[];
     public language: string;
     public plataform: string;
@@ -13,15 +13,79 @@ class Movie {
     public mainCharacterName: string;
     public producer: Professional[];
     public distributor: string;
-    public genre: Professional [];
+    public genre: string;
 
-    constructor (title:string, releaseYear:number, nacionality: Professional[], genre:Professional[])
+    constructor (title:string, releaseYear:number, nacionality: string, genre:string)
     {
         this.title = title;
         this.releaseYear = releaseYear;
         this.nacionality = nacionality;
         this.genre = genre
 
+    }
+
+    public setactors(actor: Professional[]):void {
+        this.actors = actor;
+    }
+
+    public getactors(){
+        return this.actors;
+    }
+
+    public setwriter(writ: Professional[]):void {
+        this.writer= writ;
+    }
+
+    public getwriter(){
+        return this.writer;
+    }
+
+    public setlanguage(lengua: string):void {
+        this.language= lengua;
+    }
+
+    public getlanguage(){
+        return this.language;
+    }
+
+    public setplataform(plataforma: string):void {
+        this.plataform = plataforma;
+    }
+
+    public getplataform(){
+        return this.plataform;
+    }
+
+    public setisMCU(MCU: boolean):void {
+        this.isMCU = MCU;
+    }
+
+    public getisMCU(){
+        return this.isMCU;
+    }
+
+    public setmainCharacterName(CharacterName: string):void {
+        this.mainCharacterName = CharacterName;
+    }
+
+    public getmainCharacterName(){
+        return this.mainCharacterName;
+    }
+
+    public setproducer(productor: Professional[]):void {
+        this.producer = productor;
+    }
+
+    public getproducer(){
+        return this.producer;
+    }
+
+    public setdistributor(distribuidor: string):void {
+        this.distributor = distribuidor;
+    }
+
+    public getdistributor(){
+        return this.distributor;
     }
 
     public printAll (): void{
